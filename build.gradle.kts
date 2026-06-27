8cc71baaf63728a14f4098a35b4a9d41a9bc70c8
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2026-06-27 22:35:06 +0400

    test commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,4 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
-}
+} //new change
